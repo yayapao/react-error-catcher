c1727b71926f51eb1e5633ae4fabd250f2d5b3b6
Y-lonelY <luckyyoung666@gmail.com>
2020-09-12 15:55:31 +0800

    change build npm script

---
+++ control.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# app version
+version="0.0.1"
+app="react-error-catcher"
+
+echo "hello"
+
+function build() {
+  echo ">> The current version of $app is $version"
+  cp index.js npm/index.js
+  cp index.d.ts npm/index.d.ts
+}
+
+
+if [ "$1" == "" ]; then
+  help
+elif [ "$1" == "build" ]; then
+  build
+else
+  help
+fi
